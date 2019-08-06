aa67d4d6fc83132651acf71bf95a6e1efb4dfa45
pavell00 <mymmo88@gmail.com>
2019-08-06 17:18:55 +0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import { OrderListComponent } from './components/orders-list/orders-list.component';
+import { OrderListComponent } from './components/orders-list/orders-list.component';
 import { OrderDetailComponent } from './components/order-detail/order-detail.component';
-//import { MenuItemCreateComponent } from './menuItem-create/menuItem-create.component';
-//import { MenuListComponent } from './menu-list/menu-list.component';
+import { MenuItemCreateComponent } from './components/menuItem-create/menuItem-create.component';
+import { MenuListComponent } from './components/menu-list/menu-list.component';
 
 const routes: Routes = [
-//  { path: 'order-list', component: OrderListComponent},
+  { path: 'order-list', component: OrderListComponent},
   { path: 'order-detail', component: OrderDetailComponent},
-//  { path: 'menu-list', component: MenuListComponent},
-//  { path: 'menuItem-create', component: MenuItemCreateComponent},
+  { path: 'menu-list', component: MenuListComponent},
+  { path: 'menuItem-create', component: MenuItemCreateComponent},
   { path: '**', redirectTo: '' }
 ];
 

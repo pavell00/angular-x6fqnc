e832481679c7517a3fa4a217ec04e31237a51e23
pavell00 <mymmo88@gmail.com>
2019-08-06 23:00:43 +0300

    1 file modified

--- src/app/components/menu-list/menu-list.component.ts
+++ src/app/components/menu-list/menu-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { menuItem } from '../../models/menuItem';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'menu-list',
@@ -12,7 +14,9 @@ export class MenuListComponent implements OnInit {
   menulist : menuItem[] = [];
   displayedColumns = ['id','name', 'price', 'qty', 'discount', 'Actions'];
   
-  constructor(private dataService: DataService, private router : Router) { }
+  constructor(private dataService: DataService, private router : Router,
+    private firestore: AngularFirestore,
+    private toastr:ToastrService) { }
 
   ngOnInit() {
     this.dataService.getMenuList().subscribe(actionArray => {
@@ -28,5 +32,12 @@ export class MenuListComponent implements OnInit {
   createMenuItem() {
     //menuItem-create
     this.router.navigateByUrl('menuItem-create');
+  }
+
+  onDelete(id: string) {
+    if (confirm("Are you sure to delete this record?")) {
+      this.firestore.doc('menulist/' + id).delete();
+      this.toastr.warning('Deleted successfully','EMP. Register');
+    }
   }
 }

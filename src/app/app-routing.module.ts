3b60510770790b1f4bb83f36326cf0590c817388
pavell00 <mymmo88@gmail.com>
2019-08-06 17:07:44 +0300

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //import { OrderListComponent } from './components/orders-list/orders-list.component';
-//import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { OrderDetailComponent } from './components/order-detail/order-detail.component';
 //import { MenuItemCreateComponent } from './menuItem-create/menuItem-create.component';
 //import { MenuListComponent } from './menu-list/menu-list.component';
 
 const routes: Routes = [
 //  { path: 'order-list', component: OrderListComponent},
-//  { path: 'order-detail', component: OrderDetailComponent},
+  { path: 'order-detail', component: OrderDetailComponent},
 //  { path: 'menu-list', component: MenuListComponent},
 //  { path: 'menuItem-create', component: MenuItemCreateComponent},
   { path: '**', redirectTo: '' }

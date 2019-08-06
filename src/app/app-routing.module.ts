e24cacbd49f3d422ee57c0ea90db1075922e80cd
pavell00 <mymmo88@gmail.com>
2019-08-06 16:23:49 +0300

    1 file modified, 1 added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+//import { OrderListComponent } from './orders-list/orders-list.component';
+//import { OrderDetailComponent } from './order-detail/order-detail.component';
+//import { MenuItemCreateComponent } from './menuItem-create/menuItem-create.component';
+//import { MenuListComponent } from './menu-list/menu-list.component';
+
+const routes: Routes = [
+//  { path: 'order-list', component: OrderListComponent},
+//  { path: 'order-detail', component: OrderDetailComponent},
+//  { path: 'menu-list', component: MenuListComponent},
+//  { path: 'menuItem-create', component: MenuItemCreateComponent},
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http'; 
 import { FormsModule } from '@angular/forms';
 
+import { MatTableModule, MatExpansionModule, MatFormFieldModule,
+  MatInputModule, MatButtonModule, MatCheckboxModule, MatOptionModule, MatSelectModule, 
+  MatListModule, MatIconModule, MatToolbar, MatToolbarModule, MatSnackBarModule } from '@angular/material';
+
+import { environment } from './environment';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule 
+    ],
+  declarations: [ 
+    AppComponent,
+    HelloComponent 
+    ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

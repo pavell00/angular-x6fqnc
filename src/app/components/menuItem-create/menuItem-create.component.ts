177d91cd4b71c23dcfe922813949a2c5299137ea
pavell00 <mymmo88@gmail.com>
2019-08-06 21:43:14 +0300

    2 files modified

--- src/app/components/menuItem-create/menuItem-create.component.ts
+++ src/app/components/menuItem-create/menuItem-create.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { menuItem } from '../../models/menuItem';
+import { NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'menuItem-create',
@@ -14,10 +17,13 @@ export class MenuItemCreateComponent implements OnInit {
     qty: number = 1;
     discount: number = 0;
 
-    constructor(private dataService: DataService) { }
+    constructor(private dataService: DataService,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService) { }
 
     ngOnInit(): void {
         //this.dataService.items.subscribe(res => this.list = res)
+        this.resetForm();
         this.getMaxId();
     }
 
@@ -32,4 +38,16 @@ export class MenuItemCreateComponent implements OnInit {
         //    data => (this.id = data + 1)
         //)
     }
+
+    resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+      this.dataService.formData = {
+      id: null,
+      fullName: '',
+      position: '',
+      empCode: '',
+      mobile: '',
+    }
+  }
 }

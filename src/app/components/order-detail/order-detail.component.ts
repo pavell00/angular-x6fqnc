252db3854e044b16383942fdebdd5b03db35ee5f
pavell00 <mymmo88@gmail.com>
2019-08-12 21:42:52 +0300

    2 files modified

--- src/app/components/order-detail/order-detail.component.ts
+++ src/app/components/order-detail/order-detail.component.ts
@@ -37,6 +37,23 @@ export class OrderDetailComponent implements OnInit {
     private firestore: AngularFirestore, public dialog: MatDialog) {
       this.route.queryParams.subscribe(params => {
             this.orderId = params["orderid"];
+            if (this.orderId) {
+              this.selectedMenu = null;
+              let cityRef = this.firestore.collection('orders');
+              let q = cityRef.get().toPromise().then(
+                result => result.query.where('id', '==', this.orderId).get().then(
+                  q => {
+                    cityRef.doc(this.orderId).collection("lines").get().toPromise().then(
+                    snapshot => {
+                      const v = snapshot.docs.map(
+                        w => { 
+                            console.log(w.data())
+                        }
+                      )
+                    }
+                    )
+                  }
+                ))}
         });
     }
 

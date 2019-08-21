ed27ef9561b93844b5c49824f390036d75abd28e
pavell00 <mymmo88@gmail.com>
2019-08-21 17:46:08 +0300

    1 file modified, 1 added

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -1,9 +1,12 @@
 export class Order {
-    id?: string;
-    TableNo?: string = '1';
-    OrderDate?: String;
-    isDone?: boolean = false;
-    OrderText?: string = '';
-    sumOrder?: number = 0;
-    discountOrder?: number = 0;
-  }
+  id?: string;
+  TableNo?: string = '1';
+  OrderDate?: String;
+  isDone?: boolean = false;
+  OrderText?: string = '';
+  sumOrder?: number = 0;
+  discountOrder?: number = 0;
+  check?: string = '';
+  guests?: number = 1;
+  printTime: string = '0:00';
+}
---
+++ src/app/models/prgParams.ts
@@ -0,0 +1,19 @@
+export class prgParams {
+  cassir: string;
+  waiter: string;
+  headerStr1: string;
+  headerStr2: string;
+  headerStr3: string;
+  headerStr4: string;
+  headerStr5: string;
+  headerStr6: string;
+  headerStr7: string;
+  lineStr: string;
+  tableHeader1: string;
+  totalStr: string;
+  discountStr: string;
+  serviceStr: string;
+  totalToPay: string;
+  grivnaStr: string;
+  footerStr: string;
+}
